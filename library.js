5f72a4461bc3a88f0aad13a135f3fcdb03066fb0
erlgo <erlgo@ipc6.com>
2014-12-09 06:19:03 +0800

    finish work to v0.5-0.6+

--- library.js
+++ library.js
@@ -1,5 +1,30 @@
-module.exports = {
-	init: function () {
-		return;
-	}
-};
+"use strict";
+var winston = module.parent.require('winston'),
+	async = module.parent.require('async'),
+	meta = module.parent.require('./meta');
+(function (googlefont) {
+	googlefont.init = function (params, callback) {
+		async.waterfall([
+			function (next) {
+				if (meta.config.useCustomJS != '1') {
+					meta.configs.set('useCustomJS', '1', next);
+				} else {
+					next();
+				}
+			},
+			function (next) {
+				if (meta.config.customJS && meta.config.customJS.indexOf('fonts.useso.com') > 0) {
+					next();
+				} else {
+					meta.configs.set('customJS', "<link href='http://fonts.useso.com/css?family=Open+Sans:300,400,600&subset=latin,latin-ext' rel='stylesheet'>" + (meta.config.customJS ? meta.config.customJS : ''), function (err) {
+						next();
+					});
+				}
+			}
+		], function (err) {
+			winston.info('[gootle-font-pass]', 'used:customJS', meta.config.customJS);
+			callback();
+		});
+	};
+
+}(module.exports));
