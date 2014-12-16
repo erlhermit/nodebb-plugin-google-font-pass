6013732370bf5f0f62d22cfaea1baeaaf95bb6b1
erlgo <erlgo@ipc6.com>
2014-12-16 13:00:53 +0800

    use anther way to replace lavender google fonts less file

--- library.js
+++ library.js
@@ -2,27 +2,36 @@
 var winston = module.parent.require('winston'),
 	async = module.parent.require('async'),
 	meta = module.parent.require('./meta');
+
+	var fs = require('fs');
+
+
 (function (googlefont) {
+	googlefont.replaceFonts = function (path,callback){
+		fs.exists(path, function (exists) {
+			if(exists){
+				fs.readFile(path,'utf8', function (err, data) {
+					if (err) throw err;
+					data = data.replace('fonts.googleapis.com/css?family=Raleway:100,300,700,900,500','fonts.useso.com/css?family=Open+Sans:300,400,600')
+					fs.writeFile(path,data,'utf8',function(err){
+						if (err) throw err;
+						callback();
+					});
+				});
+			}else{
+				callback();
+			}
+		});
+	};
+	var customJS = '';
 	googlefont.init = function (params, callback) {
+
 		async.waterfall([
 			function (next) {
-				if (meta.config.useCustomJS != '1') {
-					meta.configs.set('useCustomJS', '1', next);
-				} else {
-					next();
-				}
-			},
-			function (next) {
-				if (meta.config.customJS && meta.config.customJS.indexOf('fonts.useso.com') > 0) {
-					next();
-				} else {
-					meta.configs.set('customJS', "<link href='http://fonts.useso.com/css?family=Open+Sans:300,400,600&subset=latin,latin-ext' rel='stylesheet'>" + (meta.config.customJS ? meta.config.customJS : ''), function (err) {
-						next();
-					});
-				}
+				var fontspath = './node_modules/nodebb-theme-lavender/less/whiteplum.less';
+				googlefont.replaceFonts(fontspath,next);
 			}
 		], function (err) {
-			winston.info('[gootle-font-pass]', 'used:customJS', meta.config.customJS);
 			callback();
 		});
 	};
